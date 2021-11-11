777f8833b8e0e918fe7bd3cab9ecd0707282fde9
Michael Feld <mfeld1125@gmail.com>
2021-11-11 18:03:25 +0100

    finishing mixedMessages

--- portfolio/mixedMessages/main.js
+++ portfolio/mixedMessages/main.js
@@ -1,8 +1,51 @@
 
 
-const createMessage = (verb1, verb2, verb3) => {
-    let message = `When you ${verb1} and ${verb2}, you always ${verb3} too`
+
+let verbs = [
+    'abide', 
+    'arise', 
+    'awake', 
+    'be', 
+    'bear', 
+    'beat', 
+    'beget', 
+    'begin', 
+    'bend', 'breed', 
+    'bring', 
+    'broadcast', 
+    'build', 
+    'burn', 
+    'burst', 
+    'buy', 
+    'do', 
+    'draw', 
+    'dream', 
+    'drink', 
+    'drive', 
+    'dwell', 
+    'eat', 
+    'fall', 
+    'feed', 
+    'feel', 
+    'fight'
+]
+
+const getRandVerbs = (verbs) => {
+    let randVerbs = []
+    while (randVerbs.length<3) {
+        let randKey = Math.floor(Math.random()*verbs.length)
+        let randVerb = verbs[randKey]
+        if (!randVerbs.includes(randVerb)) {
+            randVerbs.push(randVerb)
+        }
+    }
+    return randVerbs
+}
+
+const createMessage = () => {
+    let randVerbs = getRandVerbs(verbs);
+    let message = `When you ${randVerbs[0]} and ${randVerbs[1]}, you always ${randVerbs[2]} too`
     return message
 
 }
-console.log(createMessage('eat', 'exercise', 'grow'))
+console.log(createMessage())
